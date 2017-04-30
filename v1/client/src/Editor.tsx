3a3e15d5299c67e798bb861254b5aaaaf2742b89
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-04-30 14:41:17 -0400

    editors autofocus, lowercase s

--- v1/client/src/Editor.tsx
+++ v1/client/src/Editor.tsx
@@ -1,8 +1,8 @@
 import React = require("react");
 
-export default ({ text, limit, textChanged, submitClicked, placeholder="Write Something"}) => (
+export default ({ text, limit, textChanged, submitClicked, placeholder="Write something"}) => (
   <div className="editor">
-    <textarea onChange={textChanged} value={text} rows={Math.ceil(text.length/62.5) || 1} placeholder={placeholder}></textarea>
+    <textarea autoFocus onChange={textChanged} value={text} rows={Math.ceil(text.length/62.5) || 1} placeholder={placeholder}></textarea>
     <footer>
       <span>{text.length}/{limit}</span>
       {text.length > 0 && <button onClick={submitClicked}>Submit</button>}
