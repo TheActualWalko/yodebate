b709cccde96e95fb1f7ae45d16ead160ff373c67
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-05-01 09:12:22 -0400

    move deploy up a level

--- v1/client/deploy.sh
+++
@@ -1,5 +0,0 @@
-#!/bin/bash
-scp ./index.html root@sam-watkinson.com:/var/www/yodebate/;
-scp ./style.css root@sam-watkinson.com:/var/www/yodebate/;
-scp ./dist/bundle.js root@sam-watkinson.com:/var/www/yodebate/dist/bundle.js;
-scp ./dist/vendor.bundle.js root@sam-watkinson.com:/var/www/yodebate/dist/vendor.bundle.js;
