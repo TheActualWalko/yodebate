359c32ebea7223ee702fe7728afb78076f84b2bc
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-05-01 09:12:39 -0400

    add redux-logger

--- v1/client/src/store.ts
+++ v1/client/src/store.ts
@@ -1,8 +1,13 @@
 import { createStore, applyMiddleware } from "redux";
 import reducer from "./reducer";
 import thunk from "redux-thunk";
+import {createLogger} from "redux-logger";
+
+const logger = createLogger({
+  stateTransformer: x=>x.toJS()
+})
 
 export default createStore(
   reducer,
-  applyMiddleware(thunk)
+  applyMiddleware(thunk, logger)
 );
