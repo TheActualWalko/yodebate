07c9824dcdc8b3624a6b6089bb04872342a5f498
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-05-04 19:56:22 -0400

    compile server before shipping
    (oops)

--- v1/deploy.sh
+++ v1/deploy.sh
@@ -9,6 +9,9 @@ scp ./client/marc.png root@sam-watkinson.com:/var/www/yodebate/public/marc.png;
 scp ./client/dist/bundle.js root@sam-watkinson.com:/var/www/yodebate/public/dist/bundle.js;
 scp ./client/dist/vendor.bundle.js root@sam-watkinson.com:/var/www/yodebate/public/dist/vendor.bundle.js;
 
+cd ./server;
+./build.sh;
+cd ..;
 scp ./server/js/main.js root@sam-watkinson.com:/var/www/yodebate/server/js/main.js;
 scp ./server/js/main.js.map root@sam-watkinson.com:/var/www/yodebate/server/js/main.js.map;
 scp ./server/package.json root@sam-watkinson.com:/var/www/yodebate/server/;
