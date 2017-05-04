63f93fbb70daf5d3ef13e454c1d7aa7e086d591f
Sam Watkinson <sam.ws.watkinson@gmail.com>
2017-05-04 19:41:42 -0400

    update deployment script

--- v1/deploy.sh
+++ v1/deploy.sh
@@ -2,13 +2,13 @@
 cd ./client;
 ./build.sh;
 cd ..;
-scp ./client/index.html root@sam-watkinson.com:/var/www/yodebate/index.html;
-scp ./client/style.css root@sam-watkinson.com:/var/www/yodebate/style.css;
-scp ./client/sam.png root@sam-watkinson.com:/var/www/yodebate/sam.png;
-scp ./client/marc.png root@sam-watkinson.com:/var/www/yodebate/marc.png;
-scp ./client/dist/bundle.js root@sam-watkinson.com:/var/www/yodebate/dist/bundle.js;
-scp ./client/dist/vendor.bundle.js root@sam-watkinson.com:/var/www/yodebate/dist/vendor.bundle.js;
+scp ./client/index.html root@sam-watkinson.com:/var/www/yodebate/public/index.html;
+scp ./client/style.css root@sam-watkinson.com:/var/www/yodebate/public/style.css;
+scp ./client/sam.png root@sam-watkinson.com:/var/www/yodebate/public/sam.png;
+scp ./client/marc.png root@sam-watkinson.com:/var/www/yodebate/public/marc.png;
+scp ./client/dist/bundle.js root@sam-watkinson.com:/var/www/yodebate/public/dist/bundle.js;
+scp ./client/dist/vendor.bundle.js root@sam-watkinson.com:/var/www/yodebate/public/dist/vendor.bundle.js;
 
-scp ./server/js/main.js root@sam-watkinson.com:/var/www/yodebate-server/js/main.js;
-scp ./server/js/main.js.map root@sam-watkinson.com:/var/www/yodebate-server/js/main.js.map;
-scp ./server/package.json root@sam-watkinson.com:/var/www/yodebate-server/;
+scp ./server/js/main.js root@sam-watkinson.com:/var/www/yodebate/server/js/main.js;
+scp ./server/js/main.js.map root@sam-watkinson.com:/var/www/yodebate/server/js/main.js.map;
+scp ./server/package.json root@sam-watkinson.com:/var/www/yodebate/server/;
